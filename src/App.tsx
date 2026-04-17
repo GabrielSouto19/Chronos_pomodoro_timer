7eeee7bcda34ae4ee8b03925291e65e540a361a2
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2026-04-17 00:59:43 -0300

    add folders to insert screenshots

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
 export function App() {
   return <div>
-    <Heading attr="123"> Nome</Heading >
-    <Heading> Nome</Heading >
+    <Heading > 
+      
+      Chronos
+      <button>
+        <TimerIcon/>
+      </button>
+
+    </Heading >
+  
 
   </div>
 }
