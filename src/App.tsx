e816d8b8e1f90827f0165d87c97b81e608aa5c08
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2026-04-17 10:26:58 -0300

    finish Container Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { Container } from './components/Container'
 import { TimerIcon } from 'lucide-react'
 
 export function App() {
-  return <div>
-    <Heading > 
+  return <>
+    {/* <Heading > 
       
       Chronos
       <button>
@@ -13,9 +14,36 @@ export function App() {
       </button>
 
     </Heading >
-  
+   */}
 
-  </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+
+      
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+
+
+  </>
 }
 
 
